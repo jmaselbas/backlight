cf025413336eaf74f9e4aa032d411c0dc230cb6b
Jules Maselbas <jmaselbas@kalray.eu>
2021-03-01 00:26:37 +0100

    Rewrite
    Add support for directly setting a value with '=';
    Add support for using percentage instead of absolute scale.
    Both can be used at the same time.
    Now the value is clamp to 0 and to the maximum value, so
    the setting is not discarded by the driver.

--- backlight.c
+++ backlight.c
@@ -1,29 +1,84 @@
 #include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
+#include <err.h>
+
+static int
+get(const char *path)
+{
+	FILE *f;
+	int ret, val;
+
+	f = fopen(path, "r");
+	if (!f)
+		err(1, "%s", path);
+
+	ret = fscanf(f, "%d", &val);
+	if (ret < 0)
+		err(1, "%s", path);
+
+	fclose(f);
+	return val;
+}
+
+static void
+set(const char *path, int val)
+{
+	FILE *f;
+	int ret;
+
+	f = fopen(path, "w");
+	if (!f)
+		err(1, "%s", path);
+
+	ret = fprintf(f, "%d", val);
+	if (ret < 0)
+		err(1, "%s", path);
+
+	fclose(f);
+}
 
 int
 main(int argc, char **argv)
 {
-	FILE *f = fopen("/sys/class/backlight/intel_backlight/brightness", "r+");
-	int val, ret, inc;
+	const char *val_path = "/sys/class/backlight/intel_backlight/brightness";
+	const char *max_path = "/sys/class/backlight/intel_backlight/max_brightness";
+	FILE *f;
+	int val, max, mode;
+	long arg;
+	char *n, *p;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: backlight [VALUE]\n");
-		return 1;
-	}
-	inc = atoi(argv[1]);
-
-	if (!f) {
-		perror("fopen");
-		return 1;
+	if (argc >= 2 && argv[1][0] == '-' && argv[1][1] == 'h') {
+		printf("usage: backlight [-h] [[=]VALUE[%]]\n");
+		return 0;
 	}
 
-	ret = fscanf(f, "%d", &val);
-	if (ret < 0)
-		perror("fscanf");
-	ret = fprintf(f, "%d", val + inc);
-	if (ret < 0)
-		perror("fprintf");
+	max = get(max_path);
+	val = get(val_path);
 
-	return ret;
+	if (argc >= 2) {
+		n = argv[1];
+		if (n[0] == '=') {
+			arg = strtol(n + 1, &p, 10);
+			if (arg == LONG_MAX || (n + 1) == p)
+				errx(1, "Not a valid number");
+			if (p && p[0] == '%')
+				arg = (max * arg) / 100;
+			val = arg;
+		} else {
+			arg = strtol(n, &p, 10);
+			if (arg == LONG_MAX || n == p)
+				errx(1, "Not a valid number");
+			if (p && p[0] == '%')
+				arg = (max * arg) / 100;
+			val += arg;
+		}
+		val = (val > max) ? max : val;
+		val = (val < 0) ? 0 : val;
+		set(val_path, val);
+	} else {
+		printf("%d\n", val);
+	}
+
+	return 0;
 }
